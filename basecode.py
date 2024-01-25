9dc526e6689362a5731204fad94a8feb84d81d34
OM MAKH <94157617+ommakh@users.noreply.github.com>
2024-01-25 14:31:31 +0530

    Add files via upload

---
+++ basecode.py
@@ -0,0 +1,68 @@
+from langchain.llms import GooglePalm
+from langchain.schema import document
+from langchain.vectorstores import FAISS
+from langchain.document_loaders.csv_loader import CSVLoader
+import langchain_community
+from langchain.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
+#from main2 import predictor
+
+api_key = 'AIzaSyAab8rjcQRq1Vt944WLj6XwqwROVVitVvQ'
+print("we are good...")
+
+llm = GooglePalm(google_api_key=api_key)
+
+
+from langchain.document_loaders.csv_loader import CSVLoader
+
+from langchain.embeddings import HuggingFaceInstructEmbeddings
+from langchain.vectorstores import FAISS
+
+instructor_embeddings = HuggingFaceInstructEmbeddings()
+vectordb_file_path = "vectordata3"
+
+
+def create_vector_db():
+    from langchain_community.document_loaders import PyPDFLoader
+
+    loader = PyPDFLoader(file_path='48lawsofpower.pdf' )
+    datx = loader.load()
+
+
+    print(datx)
+    vectordb = FAISS.from_documents(documents=datx, embedding=instructor_embeddings)
+    vectordb.save_local("vectordata3")
+
+
+
+
+
+def get_chain():
+    vectordb = FAISS.load_local(vectordb_file_path, instructor_embeddings)
+    retrival = vectordb.as_retriever(score_threshold=0.3)
+    prompt_template = """Given the following context and a question, generate an answer based on this context only.
+       In the answer try to provide as much text as possible from "response" section in the source document context without making much changes.
+       If the answer is not found in the context, kindly state "I don't know." Don't try to make up an answer.
+
+       CONTEXT: {context}
+
+       QUESTION: {question}"""
+
+    PROMPT = PromptTemplate(
+        template=prompt_template, input_variables=["context", "question"]
+    )
+
+    chain = RetrievalQA.from_chain_type(llm=llm,
+                                        chain_type="stuff",
+                                        retriever=retrival,
+                                        input_key="query",
+                                        return_source_documents=True,
+                                        chain_type_kwargs={"prompt": PROMPT})
+
+    return chain
+
+
+if __name__ == "__main__":
+    create_vector_db()
+    chain = get_chain()
+    print(chain(""))
---
+++ streamlit_app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+from basecode import get_chain,create_vector_db
+from main2 import predictor
+
+st.title(" Ask me Questions")
+
+question = st.text_input("question: ")
+st.button("enter")
+
+
+if question:
+    chain = get_chain()
+    response = chain(question)
+
+    st.header("answer")
+    st.write(response["result"])
+
+
+
